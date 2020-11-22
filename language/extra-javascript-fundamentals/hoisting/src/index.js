6e699b1def55a50df1cfb6f977bb814d69dd18f8
Yolanda Vergara <yv.avila@gmail.com>
2020-11-22 17:20:40 +0100

    fix hoisting exercise

--- language/extra-javascript-fundamentals/hoisting/src/index.js
+++ language/extra-javascript-fundamentals/hoisting/src/index.js
@@ -13,18 +13,18 @@ function f() {
 
 // Apartado B: ¿Y ahora?
 
-// var a = 1;
+ var a = 1;
 
-// (function() {
-//   console.log(a); // intenta mostrar el resultado de a pero al estar declarada después, resulta undefined
-//   var a = 2;
-//   b = 4;
-//   var c = 3;
-// })();
+ (function() {
+   console.log(a); // intenta mostrar el resultado de a pero al estar declarada después, resulta undefined
+   var a = 2;
+  b = 4;
+   var c = 3;
+ })();
 
-// console.log(a); // este console.log muestra el valor de a que tiene antes de la función
-// console.log(b); // la variable está definida de tal manera que se considera global por lo que se accede tanto dentro como fuera
-// console.log(c); // la variable c sí se declara dentro de la función pero también vive en el ámbito de la misma por lo que al no conocerla, muestra error la consola
+ console.log(a); // este console.log muestra el valor de a que tiene antes de la función
+ console.log(b); // la variable está definida de tal manera que se considera global por lo que se accede tanto dentro como fuera
+ console.log(c); // la variable c sí se declara dentro de la función pero también vive en el ámbito de la misma por lo que al no conocerla, muestra error la consola
 
 // Apartado C: ¿Y con esta ligera variación?
 
