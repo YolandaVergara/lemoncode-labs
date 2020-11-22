8b12fbac82780d1110346cd165b1f0f69937cced
Yolanda Vergara <yv.avila@gmail.com>
2020-11-22 19:43:43 +0100

    add reverseText exercise

---
+++ language/extra-javascript-fundamentals/reverseText/src/index.js
@@ -0,0 +1,7 @@
+//Dado un texto cualquiera, invierte el orden de las palabras.
+
+const reverseText = (string) => {
+  return console.log(string.split(' ').reverse());
+}
+
+reverseText("Dado un texto cualquiera, invierte el orden de las palabras")
