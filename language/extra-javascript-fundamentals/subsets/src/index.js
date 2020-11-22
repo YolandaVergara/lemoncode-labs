b14da8930138ae97e568eda6caa5ab84a6670b0f
Yolanda Vergara <yv.avila@gmail.com>
2020-11-22 21:02:00 +0100

    add subsets challenge

--- language/extra-javascript-fundamentals/subsets/src/index.js
+++ language/extra-javascript-fundamentals/subsets/src/index.js
@@ -1,7 +1,6 @@
-//Escribe una función que acepte un string como argumento y devuelva todas las partes finales de dicha palabra:
+// Escribe una función que acepte un string como argumento y devuelva todas las partes finales de dicha palabra:
 
 function subsets(word) {
-
   let result = [];
   for (i = 1; i < word.length; i++) {
     result.push(word.slice(i, word.length));
@@ -10,3 +9,10 @@ function subsets(word) {
 }
 
 console.log(subsets("Supercalifragilisticoespialidoso"));
+
+// Challenge: Repite el ejercicio anterior sin utilizar arrays auxiliares ni bucles for/do/while.
+
+const subsetsWithoutFor = (word) => Array(word.length - 1).fill('').map((string, i) => word.substring(i + 1));
+
+console.log(subsetsWithoutFor("Supercalifragilisticoespialidoso"));
+
